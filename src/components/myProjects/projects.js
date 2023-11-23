bb21818a8f832ae4a971692f447f722425d5aa3c
Inusha Peiris <101092263+inushaPeiris@users.noreply.github.com>
2023-11-23 22:04:23 +0530

    changing projects

--- src/components/myProjects/projects.js
+++ src/components/myProjects/projects.js
@@ -19,13 +19,6 @@ export default [
         name: "Travel360",
         github: "https://github.com/inushaPeiris/Travel360",
         demo: "https://www.youtube.com/watch?v=8BiP5rn5ohg&ab_channel=InushaPeiris"
-    },
-    {
-        id: 4,
-        img: "http://drive.google.com/uc?export=view&id=1zCYIivVDIALx98jHx3Ku8BMz1JemyAjn",
-        name: "Online Todo-List",
-        github: "https://github.com/inushaPeiris/todo-list",
-        demo: "https://inushapeiris-todo-list.netlify.app/"
-    },
+    }
     
 ]
